3e049fc71dd8c87e06f0ab634e57ab386bd54141
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2023-06-20 07:40:51 -0300

    feat: Avanco na parte de gravacao no pg

--- back-end/src/controlador/controladorUF.ts
+++ back-end/src/controlador/controladorUF.ts
@@ -1,8 +1,33 @@
 import { Request, Response } from "express";
+import { ufRepositorio } from "../repositorios/UFRepositorie";
 
 export class controladorUF{
 
     async criarUF(resquest: Request, response:Response){
-        return response.json('Funcionol');    
+        // LOGICA ERRADA POR ENQUANTO
+        try{
+            const {Codigo_UF , Sigla , Nome, Status} = resquest.body;
+
+            if(!Codigo_UF && !Sigla && !Nome && Status){
+                return response.status(400).json({mensagem: "Sigla ou Nome ja inseridos"})
+            }
+            else{
+                const novoUF = ufRepositorio.create(
+                    {Codigo_UF:Codigo_UF,
+                    Sigla:Sigla,
+                    Nome:Nome,
+                    Status: Status}
+                );
+                await ufRepositorio.save(novoUF);
+
+                return response.status(200).json(novoUF);
+            }
+            
+
+        }
+        catch(erro){
+            return response.status(500).json({mensagem: "Erro interno no servidor"})
+        }
+        
     }
 }
